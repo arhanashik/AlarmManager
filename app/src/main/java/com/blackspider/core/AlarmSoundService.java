10c5003b468162648fb47ca604bf57eb06943bcb
arhan.ashik <ashik.pstu.cse@gmail.com>
2018-05-01 01:22:41 +0600

    A simple alarm manager app

---
+++ app/src/main/java/com/blackspider/core/AlarmSoundService.java
@@ -0,0 +1,39 @@
+package com.blackspider.core;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class AlarmSoundService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Start media player
+        mediaPlayer = MediaPlayer.create(this, R.raw.alarm_sound);
+        mediaPlayer.start();
+        mediaPlayer.setLooping(true);//set looping true to run it infinitely
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //On destory stop and release the media player
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            mediaPlayer.release();
+        }
+    }
+}
